d39520f02d32008a1b2d3bb963081c1d572b6954
Alexander Levadniy <aleksander.levadniy@teamaol.com>
2018-02-10 02:46:27 +0200

    Move to api collector snapshots

--- src/main/kotlin/com/aol/mobile/sdk/apitracker/TrackerPlugin.kt
+++ src/main/kotlin/com/aol/mobile/sdk/apitracker/TrackerPlugin.kt
@@ -33,7 +33,7 @@ import java.io.File
 
 class TrackerPlugin : Plugin<Project> {
     companion object {
-        const val API_COLLECTOR_VERSION = "1.3"
+        const val API_COLLECTOR_VERSION = "1.3-SNAPSHOT"
         const val API_ANNOTATIONS_VERSION = "1.2"
 
         const val PUBLIC_API_CLASSIFIER = "pubapi"
